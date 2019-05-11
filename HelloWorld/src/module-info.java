cb97989798ff6f4d4cc76557a7141b7e6a8e2825
patrowe <pamela.trowe@gmail.com>
2019-05-11 15:35:31 +0200

    erster commit

---
+++ HelloWorld/src/module-info.java
@@ -0,0 +1,11 @@
+module helloWorld {
+	requires javafx.base; 
+	requires javafx.controls; 
+	requires javafx.fxml; 
+	requires javafx.graphics; 
+	requires javafx.media; 
+	requires javafx.swing; 
+	requires javafx.web;
+	
+	exports helloWorld;
+}
